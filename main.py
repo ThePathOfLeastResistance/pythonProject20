21f2a96cf397a034f70b952ba077d22e53e66a58
Jamerson <jamescao800@gmail.com>
2023-03-06 22:46:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# How to Make a Function in Python
+# First, you have to wirte def function(): ("function" can be replaced with any word, it is just a place holder)
+
+def add():
+    # In here add what you want to do
+    y = 2 + 2
+    return y
+
+#to call the functin, just first the word and the brackets
+
+answer = add()
+
+print(answer)
